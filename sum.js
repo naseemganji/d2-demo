01dd60ab8d79c71a5bed0bbeaed57412efc8d896
Naseem Ganji <naseem.ganji@icloud.com>
2021-11-23 15:55:37 -0500

    updated sum.js

---
+++ sum.js
@@ -0,0 +1,18 @@
+//create a program that takes
+//any number of commnad line arguments
+// that will be numbers and adds them together
+// in  and returns a sum of all numbers
+
+//how do I add numbers
+console.log(1+2)
+// do I store multiple numbers
+const arr = [1,2,3,4,5,6,7];
+console.log("------------")
+console.log(arr)
+//how do I add these numbers in array
+for (let i = 0; i <arr.length; i++) {
+  const element = arr[i];
+  console.log("i is === ", i ," and arr[i] is === ", arr[i]);
+  //console.log("--------");
+  //console.log(i++)
+}
