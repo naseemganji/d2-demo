ceb40da62212549b27fb8fe800bb541751fa1b1e
Naseem Ganji <naseem.ganji@icloud.com>
2021-11-23 22:37:09 -0500

    sum.js completed

--- sum.js
+++ sum.js
@@ -1,13 +1,10 @@
 const sum = function(arr) {
 let result = 0
-console.log(arr)
-for (let i = 0; i <arr.length; i++) {
-  result = result+Number(arr[i])
+for (let element of arr){
+  result += Number(element);
 }
 return result;
 }
 const arr = process.argv.slice(2);
 const result = sum(arr);
 console.log(`total is === ${result}`)
-
-
