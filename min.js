bbf2276d277a85b41b0ad8b2223a5daf3df2cfbf
Naseem Ganji <naseem.ganji@icloud.com>
2021-11-25 02:39:13 -0500

    updated

--- min.js
+++ min.js
@@ -7,12 +7,15 @@ const min = function (numbers) {
   }
   return minValue;
 };
-let val1 = [1260, 490, 599, 1400, 820];
-let val2 = [-4, -1, 3, 0, 1, 4, -2];
-let val3 = [232];
-let val4 = [45, 10, -20, 0, 3, -20];
 
-console.log(min(val1));
-console.log(min(val2));
-console.log(min(val3));
-console.log(min(val4));
+const flightPrices = [1260, 490, 599, 1400, 820];
+console.log(`The cheapest flight amongst $1260, $490, $599, $1400 and $820 costs \$${min(flightPrices)}`);
+
+const golfScores = [-1, 3, 0, -4, 1, 4, -2];
+console.log(`The winning golf score amongst -1, 3, 0, -4, 1, 4 and -2 is ${min(golfScores)}`);
+
+const pageNumbers = [232];
+console.log(`The shortest book out of a list of single book with 232 pages has ${min(pageNumbers)} pages`);
+
+const temperatures = [45, 10, -20, 0, 3, -20];
+console.log(`The coldest temperature amongst 45C, 10C, -20C, 0C, 3C and -20C is ${min(temperatures)}C`);
